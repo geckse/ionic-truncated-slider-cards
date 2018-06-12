9b1d20892ba58759aefe3ee78fb28035ad14f178
Marcel Claus <m.claus@q23.de>
2018-06-13 00:23:12 +0200

    app init

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+
+import { MyApp } from './app.component';
+import { TruncatedSliderPage } from '../pages/truncated-slider/truncated-slider';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    TruncatedSliderPage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    TruncatedSliderPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
---
+++ src/pages/truncated-slider/truncated-slider.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-truncated-slider',
+  templateUrl: 'truncated-slider.html'
+})
+export class TruncatedSliderPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
